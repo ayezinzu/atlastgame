7c08a203dc84d43563d647c5a9f80039e9e0cc49
ayezinzu <59067019+ayezinzu@users.noreply.github.com>
2020-08-06 02:56:31 +0530

    fix

--- commands/boss.js
+++ commands/boss.js
@@ -14,9 +14,11 @@ exports.run = async (client, message, args) => {
   
   setInterval( async () => {
   
-  setTimeout(() => {
-    message.channel.send(`A Raid will begin in 5 minutes! Prepare for battle!`)
-  }, 300000);
+    
+
+    setTimeout(() => {
+      message.channel.send(`A Raid will begin in 5 minutes! Prepare for battle!`)
+    }, 300000);
   
   
  
